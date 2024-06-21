78a46321bf7337e34c3119462365e63a49857cd6
Ankit Bhagat <annonymous007banda@gmail.com>
2024-06-21 22:49:27 +0530

    merged the server for running the backend and frontend

--- nasa-back-end/app.js
+++ nasa-back-end/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { planetRoute } = require('./routes/index.js');
 
@@ -10,8 +11,13 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.use(planetRoute);
+
+// app.route('/').get((req, res) => {
+//     res.sendFile(path.join(__dirname, 'src', 'index.html'));
+// });
 
 app.use('*', (req, res) => {
     res.status(404).json({
